673dc19d68cef437ba186c12aea3ec0e1917f6ee
Maria Camila Lopez <mariacalosan@gmail.com>
2021-06-15 18:27:42 -0500

    se le agrego nuevos archivos y estilos a el proyecto ya no se esta utilizando bootstrap si no materialize

---
+++ aplicacion/static/aplicacion/js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+//fechas
+
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
+from .models import ventas,Cliente,atracciones,carrito,empleado,usuario
 # Create your views here.
 
 def index(request):
     return render(request, 'aplicacion/index.html')
+
