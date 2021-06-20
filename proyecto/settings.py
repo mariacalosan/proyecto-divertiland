f10158192e6f2db812384cbc747f198e8f3ec20c
Maria Camila Lopez <mariacalosan@gmail.com>
2021-06-19 20:00:51 -0500

    se arreglo las redirecciones a el logearse y desloguearse

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-LOGIN_REDIRECT_URL = reverse_lazy('/')
-LOGOUT_REDIRECT_URL = reverse_lazy('/login')
+LOGIN_REDIRECT_URL = reverse_lazy('aplicacion:index')
+LOGOUT_REDIRECT_URL = reverse_lazy('aplicacion:index')
