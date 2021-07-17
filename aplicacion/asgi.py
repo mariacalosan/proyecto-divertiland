16dcd493c9d8ad152fa9bdbfc0b7021456c590e9
Maria Camila Lopez <mariacalosan@gmail.com>
2021-07-17 11:35:17 -0500

    mover archivos de carpeta proyecto ala carpeta de aplicacion

--- aplicacion/asgi.py
+++ aplicacion/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for proyecto project.
+ASGI config for aplicacion project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplicacion.settings')
 
 application = get_asgi_application()
