fae221149cac8266a1782afe2da104c6672021bb
Maria Camila Lopez <mariacalosan@gmail.com>
2021-07-16 23:16:17 -0500

    se creo el modulo ventas se incluyeron sus modelos en el admin se crearon los modelos carrito y itemcarrito

--- apps/venta/models.py
+++ apps/venta/models.py
@@ -1,3 +1,22 @@
+from proyecto.models import AuditarModel
 from django.db import models
+from datetime import datetime
+from proyecto.models import AuditarModel
+from apps.atraccion.models import Atraccion
+from apps.usuario.models import Cliente
 
 # Create your models here.
+# clase/modelo
+class Carrito(AuditarModel):
+    # atributos
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    pagado = models.BooleanField(default=False)
+
+
+class ItemCarrito(AuditarModel):
+    atraccion = models.ForeignKey(Atraccion, on_delete=models.CASCADE)
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.carrito.cliente.usuario.username + " - " + self.atraccion.nombre
+
